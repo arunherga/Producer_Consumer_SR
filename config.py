3bd5220c95c5167a92600e53a7df5fdb38ef216b
Arunbbhat <arun.b.bhat@gmail.com>
2023-04-06 09:40:44 +0530

    modifying config file

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 config = {
-     'bootstrap.servers': 'pkc-l7pr2.ap-south-1.aws.confluent.cloud:9092',     
-     'security.protocol': 'SASL_SSL',
-     'sasl.mechanisms': 'PLAIN',
-     'sasl.username': 'BKKIAD3GHBQUHODP', 
-     'sasl.password': 'GyrqAWi2qMOZtZZhTJLu0QIKhCevuDkEnGDQpiGtursivi6TQTLzps+d/JcJfKHm'}
+     'bootstrap.servers': '<>',     
+     'security.protocol': '<>',
+     'sasl.mechanisms': '<>',
+     'sasl.username': '<>', 
+     'sasl.password': '<>'}
 
 sr_config = {
-    'url': 'https://psrc-epkz2.ap-southeast-2.aws.confluent.cloud',
-    'basic.auth.user.info':'NRNRHBIR5VT53YA3:dVqIf+PCb4X3Y+cr6cws5TAl4e2yvHrBiYhV8CuBV46EzFIyoZ9Dw5ziBCoOW4cM'
+    'url': '<>',
+    'basic.auth.user.info':'<:>'
 }
